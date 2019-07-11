83c617e258cf9c0a840b440a8989e53b43b6c46a
Joshua Veit <joshuaveit1995@gmail.com>
2019-07-11 10:15:18 -0500

    finished adding the ability for managers to view their current routes, and edit those routes (doesn't yet include Google API calls, creating new routes, or assigning routes to dirvers

--- TruckMaster-Angular/src/app/components/manager/manager-routes/manager-routes.component.ts
+++ TruckMaster-Angular/src/app/components/manager/manager-routes/manager-routes.component.ts
@@ -4,6 +4,7 @@ import { HttpErrorResponse } from '@angular/common/http';
 import { Route } from 'src/app/models/Route';
 import { CacheService } from 'src/app/services/cache.service';
 import { PendingService } from 'src/app/services/pending.service';
+import { RouteNode } from 'src/app/models/RouteNode';
 
 @Component({
   selector: 'app-manager-routes',
@@ -16,6 +17,7 @@ export class ManagerRoutesComponent implements OnInit {
   isPending: boolean = false;
 
   selectedRoute: Route = null;
+  selectedRouteIndex: number = null;
 
   constructor(
     private rService: RouteService,
@@ -50,16 +52,84 @@ export class ManagerRoutesComponent implements OnInit {
 
   selectRoute(route: Route) {
     this.selectedRoute = route;
-    console.log(route);
+    this.selectedRouteIndex = this.cache.routes.indexOf(route);
   }
 
   submit() {
-    console.log("submit");
-    this.selectedRoute = null;
+    
+    this.errorMessage = null;
+
+    // a non-negative selectedRouteIndex means that the 
+    // selectedRoute alread exists in the database and
+    // we are updating not creating it
+    if(this.selectedRouteIndex > -1) {
+
+      this.rService.updateRoute(
+        this.selectedRoute,
+        (route: Route) => {
+
+          this.cache.routes[this.selectedRouteIndex] = route;
+          this.selectedRoute = null;
+        },
+        (error: HttpErrorResponse) => {
+          this.errorMessage = error.message;
+        }
+      );
+
+    } else {
+
+      this.rService.createRoute(
+        this.selectedRoute,
+        (route: Route) => {
+          
+          this.cache.routes.push(route);
+          this.selectedRoute = null;
+        },
+        (error: HttpErrorResponse) => {
+          this.errorMessage = error.message;
+        }
+      );
+    }
   }
 
   cancel() {
-    console.log("cancel");
     this.selectedRoute = null;
+  }
+
+  moveNodeUp(node: RouteNode) {
+    
+    let index = this.selectedRoute.nodes.indexOf(node);
+
+    if(index > 0) {
+      this.selectedRoute.nodes[index] = this.selectedRoute.nodes[index - 1];
+      this.selectedRoute.nodes[index - 1] = node;
+    }
+  }
+
+  moveNodeDown(node: RouteNode) {
+
+    let index = this.selectedRoute.nodes.indexOf(node);
+
+    if((index < this.selectedRoute.nodes.length - 1) && (index > -1)) {
+      this.selectedRoute.nodes[index] = this.selectedRoute.nodes[index + 1];
+      this.selectedRoute.nodes[index + 1] = node;
+    }
+  }
+
+  deleteNode(node: RouteNode) {
+
+    let index = this.selectedRoute.nodes.indexOf(node);
+
+    if((this.selectedRoute.nodes.length > 2) && (index > -1)) {
+      this.selectedRoute.nodes.splice(index, 1);
+    }
+  }
+
+  newNode() {
+
+    this.selectedRoute.nodes.push({
+      id: null,
+      location: ""
+    });
   }
 }
